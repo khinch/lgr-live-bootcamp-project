35c469c8e1e77e64c04b6821d7bfd3f1776f557a
Kieren Hinch <khinch-github@nylonmoon.com>
2025-07-21 15:35:00 +0100

    [Sprint 1][Task 4] fix: refactor to lib
    Refactor main function into library for testability.

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,52 @@
+use axum::{serve::Serve, Router};
+use std::error::Error;
+use tokio::signal;
+use tower_http::services::ServeDir;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        // let router = todo!();
+        let router = Router::new().nest_service("/", ServeDir::new("assets"));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // todo!();
+        Ok(Application { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("Listening on {}", &self.address);
+        self.server.with_graceful_shutdown(shutdown_signal()).await
+    }
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,49 +1,12 @@
-use axum::{response::Html, routing::get, Router};
-use tokio::signal;
-use tower_http::services::ServeDir;
-
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    }
-}
+use auth_service::Application;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .nest_service("/", ServeDir::new("assets"))
-        .route("/hello", get(hello_handler));
-
-    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
-    // This is needed for Docker to work, which we will add later on.
-    // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-
-    axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal())
+    let application = Application::build("0.0.0.0:3000")
         .await
-        .unwrap();
-}
-
-async fn hello_handler() -> Html<&'static str> {
-    Html("<h1>¡Hola, mundo!</h1>")
+        .expect("Failed to build auth-service application");
+    application
+        .run()
+        .await
+        .expect("Failed to run auth-service application");
 }
