3bfe08340d1a912064677ad4768a381f58360e27
Kieren Hinch <khinch-github@nylonmoon.com>
2025-07-22 10:54:42 +0100

    [Sprint 1][Task 5] feature: Implement auth service routes and include supporting system tests

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,4 +1,4 @@
-use axum::{serve::Serve, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
 use std::error::Error;
 use tokio::signal;
 use tower_http::services::ServeDir;
@@ -10,14 +10,18 @@ pub struct Application {
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        // let router = todo!();
-        let router = Router::new().nest_service("/", ServeDir::new("assets"));
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/logout", post(logout))
+            .route("/verify-token", post(verify_token));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
 
-        // todo!();
         Ok(Application { server, address })
     }
 
@@ -49,4 +53,24 @@ async fn shutdown_signal() {
         _ = ctrl_c => {},
         _ = terminate => {},
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
