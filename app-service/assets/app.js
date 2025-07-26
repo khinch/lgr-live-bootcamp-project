a3a952fd03e05f2720d631c06b18f90c784d00aa
Kieren Hinch <khinch-github@nylonmoon.com>
2025-07-26 11:32:34 +0100

    [Sprint 1] fix: use relative paths for assets
    Description: contents of /assets were returning 404 after implementing
    routing. Use relative path, assets, instead.

--- app-service/assets/app.js
+++ app-service/assets/app.js
@@ -3,42 +3,42 @@ const logoutLink = document.getElementById("logout-link");
 const protectImg = document.getElementById("protected-img");
 
 logoutLink.addEventListener("click", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let url = logoutLink.href;
+  let url = logoutLink.href;
 
-    fetch(url, {
-        method: 'POST',
-        credentials: 'include', // This will include cookies in the request
-    }).then(response => {
-        if (response.ok) {
-            loginLink.style.display = "block";
-            logoutLink.style.display = "none";
-            protectImg.src = "/assets/default.jpg";
-        } else {
-            alert("Failed to logout");
-        }
-    });
+  fetch(url, {
+    method: "POST",
+    credentials: "include", // This will include cookies in the request
+  }).then((response) => {
+    if (response.ok) {
+      loginLink.style.display = "block";
+      logoutLink.style.display = "none";
+      protectImg.src = "assets/default.jpg";
+    } else {
+      alert("Failed to logout");
+    }
+  });
 });
 
 (() => {
-    fetch('/protected').then(response => {
-        if (response.ok) {
-            loginLink.style.display = "none";
-            logoutLink.style.display = "block";
+  fetch("/protected").then((response) => {
+    if (response.ok) {
+      loginLink.style.display = "none";
+      logoutLink.style.display = "block";
 
-            response.json().then(data => {
-                let img_url = data.img_url;
-                if (img_url !== undefined && img_url !== null && img_url !== "") {
-                    protectImg.src = img_url;
-                } else {
-                    protectImg.src = "/assets/default.jpg";
-                }
-            });
+      response.json().then((data) => {
+        let img_url = data.img_url;
+        if (img_url !== undefined && img_url !== null && img_url !== "") {
+          protectImg.src = img_url;
         } else {
-            loginLink.style.display = "block";
-            logoutLink.style.display = "none";
-            protectImg.src = "/assets/default.jpg";
+          protectImg.src = "assets/default.jpg";
         }
-    });
+      });
+    } else {
+      loginLink.style.display = "block";
+      logoutLink.style.display = "none";
+      protectImg.src = "assets/default.jpg";
+    }
+  });
 })();
